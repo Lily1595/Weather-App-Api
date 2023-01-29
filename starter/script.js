909fb59f19159690a44b56e9e4a2859a3bc2bb7a
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-29 15:06:36 +0000

    added event listener on submit button and created chosen city variable

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,21 @@
+//get api key and api address
 let APIKey = "bdd2c0c8b3b660738955be8280edc04c";
 let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
 
+//grab the elements needed from html using query selector 
 let searchForm = document.querySelector('#search-form');
 let searchInput = document.querySelector('#search-input');
 let historyList = document.querySelector('#history');
 
 //remember on forms you have to use prevent default 
+//following code adds event listener on the submit button 
+// 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  let chosenCity = searchInput.value;
+  console.log(chosenCity); //this works 
+  fetch(queryURL)
+  .then(response => response.json())
+
+
 }); 
