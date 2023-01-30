4056cf7acee883270161b6fe31ba51b09c7fe9d5
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-30 16:46:27 +0000

    altered code so that data saved in local sotrage is then appended onto the page

--- starter/script.js
+++ starter/script.js
@@ -9,23 +9,28 @@ let searchInput = document.querySelector('#search-input');
 let searchButton = document.querySelector('#search-button');
 let historyList = document.querySelector('#history');
 
-let index = 0
+//when the page loads this code retrieves any saved history buttons from local storage
+//if saved data is present, the innerHTML of history list is altered 
+window.addEventListener("load", function() {
+  let savedHistory = localStorage.getItem("historyButtons");
+  if (savedHistory) {
+    historyList.innerHTML = savedHistory;
+  }
 
-/*fetch(queryURL)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); //works 
-  })*/
+//when the search button is clicked, a new vairable is created called chosenCity which is set equal to the value of the user input 
+//a new variable called history button is created to be a clone of the search button 
+//classes are added to the history button and the text content is set as equal to the chosenCity variable 
+//then the history button is appended to the historyList 
+  searchButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    let chosenCity = searchInput.value;
+    let historyButton = searchButton.cloneNode(true);
+    historyButton.classList.add("history-button", "bg-light", "w-100", "btn", "mt-3", "text-dark");
+    historyButton.textContent = chosenCity;
+    historyList.appendChild(historyButton);
 
-//following code adds event listener on the search button. when it's clicked the default behaviour of a form is prevented. a new variable called chosenCity is created and set to the value of the searchInput element. Then a new history button element is created by cloning the search button. the text content of the history button is set to the chosenCity variable and then the history buttons are appended to the history list. The textContent of all the history button elements is then stored in localStorage with index as the key.
-
-searchButton.addEventListener("click", function (e) {
-  e.preventDefault();
-  let chosenCity = searchInput.value;
-  let historyButton = searchButton.cloneNode(true);
-  historyButton.classList.add("history-button", "bg-light", "w-100", "btn", "mt-3", "text-dark");
-  historyButton.textContent = chosenCity;
-  historyList.appendChild(historyButton);
-  localStorage.setItem(index, historyButton.textContent);
+// Store history buttons in local storage
+    localStorage.setItem("historyButtons", historyList.innerHTML);
+  });
 });
 
